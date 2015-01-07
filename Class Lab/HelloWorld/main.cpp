88c0d742ecceac753f88821183a37b06346ff7cb
27Raul <rmata3@student.rcc.edu>
2015-01-06 20:06:43 -0800

    fh

---
+++ Class Lab/HelloWorld/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Raul Mata
+ * Purpose: Our First Project
+ * Created on January 6, 2015, 6:05 PM
+ */
+
+//system libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//global constants
+
+//Function prototypes
+
+//execution begins here!
+
+int main(int argc, char** argv) {
+//Output Hello World
+    cout<<"Hello World"<<endl;
+    //exit stage right!
+    return 0;
+}
+
